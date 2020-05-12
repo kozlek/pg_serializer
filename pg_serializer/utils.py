a12c0d518c545059a0dbc8562653a8a0abe249ae
Thomas Berdy <thomas.berdy@outlook.com>
2020-05-12 09:14:02 +0200

    doc: add docstring to generate_custom_select_with_params() function

--- pg_serializer/utils.py
+++ pg_serializer/utils.py
@@ -1,6 +1,6 @@
 from contextlib import contextmanager
 from functools import reduce
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 import psycopg2.extensions
 from django.db import connection, models
@@ -12,7 +12,23 @@ def dict_merge(*dicts: List[Dict]) -> Dict:
 
 def generate_custom_select_with_params(
     queryset: models.QuerySet, annotations: Dict[str, models.Expression]
-):
+) -> Tuple[str, Tuple]:
+    """Force Django to generate a custom select statement.
+    Using internal query API:
+    1) clear out all the default queryset fields, and remove added annotations
+    2) recreate the internal annotations register using the provided ones
+    3) generate sql + params to escape. params will be escaped by psycopg2 later.
+
+    Parameters
+    ----------
+    queryset: models.QuerySet
+    annotations: Dict[str, models.Expression]
+
+    Returns
+    -------
+    Tuple[str, Tuple]
+
+    """
     queryset.query.default_cols = False
     queryset.query.annotations = {}
 
